b2e24eaab1e2528da89358b3cc8d408d61964d86
GitHub Action <action@github.com>
2025-10-07 12:00:40 +0000

    Release v0.2.15

---
+++ dist/module.d.mts
@@ -0,0 +1,8 @@
+import * as _nuxt_schema from '@nuxt/schema';
+
+interface ModuleOptions {
+    isEnabled?: boolean;
+}
+declare const _default: _nuxt_schema.NuxtModule<ModuleOptions, ModuleOptions, false>;
+
+export { _default as default };
---
+++ dist/runtime/dummy-composables/authDummy.js
@@ -0,0 +1,30 @@
+export function useAuth() {
+  const status = "unauthenticated";
+  const data = void 0;
+  const lastRefreshedAt = void 0;
+  function getCsrfToken() {
+    return "dummy";
+  }
+  function getProviders() {
+    return [];
+  }
+  const getSession = (_) => {
+    return Promise.resolve({});
+  };
+  const signIn = (_, __, ___, ____) => {
+    return Promise.resolve({ error: null, status: 200, ok: true, url: "/auth/signin" });
+  };
+  const signOut = (_) => {
+    return Promise.resolve(void 0);
+  };
+  return {
+    status,
+    data,
+    lastRefreshedAt,
+    getCsrfToken,
+    getProviders,
+    getSession,
+    signIn,
+    signOut
+  };
+}
